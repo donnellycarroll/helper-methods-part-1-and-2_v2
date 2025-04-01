b8c61e8345defefe153e0e2d23806b83e69e57b0
donnellycarroll <146589442+donnellycarroll@users.noreply.github.com>
2025-04-01 23:41:46 +0000

    messed somehting up maybe

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,9 +22,8 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.new
-    @movie.title = params.fetch(:title)
-    @movie.description = params.fetch(:description)
+    movie_attributes = params.require(:movie).permit(:title, :description)
+    @movie = Movie.new(movie_attributes)
 
     if @movie.valid?
       @movie.save
@@ -35,20 +34,13 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    the_id = params.fetch(:id)
-
-    matching_movies = Movie.where({ id: the_id })
-
-    @movie = matching_movies.first
+    @movie = Movie.find(params.fetch(:id))
 
   end
 
   def update
-    the_id = params.fetch(:id)
-    movie = Movie.where({ id: the_id }).first
-
-    movie.title = params.fetch("query_title")
-    movie.description = params.fetch("query_description")
+    movie_attributes = params.require(:movie).permit(:title, :description)
+    @movie = Movie.new(movie_attributes)
 
     if movie.valid?
       movie.save
