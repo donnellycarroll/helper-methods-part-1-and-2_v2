5e367441cbbd5015c8aa1abd0586abae4e121de5
donnellycarroll <146589442+donnellycarroll@users.noreply.github.com>
2025-04-01 22:29:12 +0000

    refactoring?

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,6 @@
 class MoviesController < ApplicationController
   def new
     @the_movie = Movie.new
-
-    render template: "movies/new"
   end
 
   def index
@@ -16,7 +14,7 @@ class MoviesController < ApplicationController
       end
 
       format.html do
-        render({ template: "movies/index" })
+        
       end
     end
   end
@@ -28,7 +26,6 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render({ template: "movies/show" })
   end
 
   def create
@@ -51,7 +48,6 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render({ template: "movies/edit" })
   end
 
   def update
